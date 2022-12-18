1b6266d8ee25c4aae43ddc5b82d3efedec533a93
Admin <kavyak6999@gmail.com>
2022-12-18 11:31:49 +0530

    assignment operator

---
+++ operator/operator.js
@@ -0,0 +1,12 @@
+var a=parseInt(prompt("enter the a value"));
+var b=parseInt(prompt("enter the b value"));
+//var z = a+=b;
+//alert(z);
+//a-=b;
+//alert(a);
+/*a*=b;
+alert(a);
+a/=b;
+alert(a);
+a%=b;
+alert(a);*/
